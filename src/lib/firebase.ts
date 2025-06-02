3085a92db8d72d717f5b126e0071691344e920d9
Your Name <your.email@example.com>
2025-06-03 02:30:42 +0300

    Environment variables için Firebase config güncellendi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsJryrYaMl-umM2tLNESCgLjUBWC3QNZI",
-  authDomain: "konusmak-ister-misin.firebaseapp.com",
-  databaseURL: "https://konusmak-ister-misin-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "konusmak-ister-misin",
-  storageBucket: "konusmak-ister-misin.appspot.com",
-  messagingSenderId: "433699843658",
-  appId: "1:433699843658:web:26f92c2bc3914adedb40b3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
